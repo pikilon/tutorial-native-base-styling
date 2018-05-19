e918c2de4a65b1a9d9c9cc43e06d6458f954f605
unknown <pikilon>
2018-05-19 14:33:17 +0200

    The native components are transparent

--- MyComponent.style.js
+++ MyComponent.style.js
@@ -3,10 +3,8 @@ import variable from "./native-base-theme/variables/platform";
 export const STYLE_NAME = 'Piki.MyComponent';
 
 export const generateStyle = (variables = variable) => ({
-  'NativeBase.ViewNB': {
-    'NativeBase.Text':{
-      color: variables.brandPrimary      
-    },
+  'NativeBase.Text':{
+    color: variables.brandPrimary      
   },
   nativeComponent: {
     flex: 1,
