8e94c14e9cb1c9daa72bcbd4e5ef9a5337ac42e6
unknown <pikilon>
2018-05-19 14:21:16 +0200

    Connect tags with styles

--- MyComponent.js
+++ MyComponent.js
@@ -1,16 +1,15 @@
 import React from 'react';
-import { StyleSheet, Text } from 'react-native';
-import { connectStyle, View } from 'native-base';
+import { StyleSheet } from 'react-native';
+import { connectStyle, View, Text } from 'native-base';
 import { STYLE_NAME } from './MyComponent.style'
 
 class MyComponentWithoutStyles extends React.Component { // doesn't work with functional
   render() {
-      const { container, text } = this.props.style;
     return (
-        <View style={container}>
-            <Text style={text}>Open up App.js to start working on your app!</Text>
-            <Text style={text}>Changes you make will automatically reload.</Text>
-            <Text style={text}>Shake your phone to open the developer menu.</Text>
+        <View>
+            <Text>Open up App.js to start working on your app!</Text>
+            <Text>Changes you make will automatically reload.</Text>
+            <Text>Shake your phone to open the developer menu.</Text>
         </View>
     );
   }
--- MyComponent.style.js
+++ MyComponent.style.js
@@ -3,15 +3,16 @@ import variable from "./native-base-theme/variables/platform";
 export const STYLE_NAME = 'Piki.MyComponent';
 
 export const generateStyle = (variables = variable) => ({
-  container: {
+  'NativeBase.ViewNB': {
     flex: 1,
-    backgroundColor: 'orange',
+    backgroundColor: 'white',
     alignItems: 'center',
     justifyContent: 'center',
+
+    'NativeBase.Text':{
+      color: variables.brandPrimary      
+    },
   },
-  text: {
-    color: variables.brandPrimary
-  }
 });
 
 export const styleRule = (variables = variable) => ({
