50135f3502fe01d534d1bfed5fcdaa4c11281360
Francisco Gutierrez Saraza <frgu@ciklum.com>
2018-05-21 15:19:44 +0200

    Vertical  menu styles

--- components/VerticalMenu.style.js
+++ components/VerticalMenu.style.js
@@ -1,11 +1,22 @@
 import variable from "../native-base-theme/variables/platform";
+import { STYLE_NAME as myButtonSelector } from './MyButton.style';
 
 export const STYLE_NAME = 'Piki.VerticalMenu';
 
-export const generateStyle = (variables = variable) => ({
-  'NativeBase.ViewNB': {
+export const generateStyle = (variables = variable) => {
+  const marginTop = variables.DefaultFontSize / 1.5;
+  return ({
+    'NativeBase.ViewNB': {
+
+      [myButtonSelector]: {
+      'NativeBase.Button':{
+        alignSelf: 'stretch',
+        marginTop,
+
+      },
+    },
   },
-});
+});}
 
 export const styleRule = (variables = variable) => ({
 	[STYLE_NAME]: generateStyle(variables)
