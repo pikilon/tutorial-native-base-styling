7d7effc959055453da615c48ab54ef2e948fd39f
Francisco Gutierrez Saraza <frgu@ciklum.com>
2018-05-21 15:04:01 +0200

    Horizontal menu styles

--- components/HorizontalMenu.style.js
+++ components/HorizontalMenu.style.js
@@ -1,11 +1,29 @@
 import variable from "../native-base-theme/variables/platform";
-
+import { STYLE_NAME as myButtonSelector } from './MyButton.style';
 export const STYLE_NAME = 'Piki.HorizontalMenu';
 
-export const generateStyle = (variables = variable) => ({
-  'NativeBase.ViewNB': {
+export const generateStyle = (variables = variable) => {
+  const marginLeft = variables.DefaultFontSize / 3;
+  return ({
+    'NativeBase.ViewNB': {
+      paddingTop: 16,
+      paddingBottom: 8,
+      flexDirection: 'row',
+
+      [myButtonSelector]: {
+      'NativeBase.Button':{
+         paddingHorizontal: marginLeft,
+         marginLeft,
+      },
+      // The first button without margin
+      '.first': {
+        'NativeBase.Button':{
+          marginLeft: 0,
+        },
+      },
+    },
   },
-});
+});}
 
 export const styleRule = (variables = variable) => ({
 	[STYLE_NAME]: generateStyle(variables)
